ce90cd846deb439ad66d9a6774e9647f1ca97bcd
Patrick Kawika Reveira <patrick.k.reveira@gmail.com>
2024-03-08 13:36:31 -0800

    fixed issues after componentizing

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const app = express();
 const dotenv = require('dotenv');
 
 dotenv.config(); //loads our environment variables from our .env file
-
+app.use(cors());
+app.use(express.json());
+console.log(process.env.MOVIES_URL);
 const { handleMovieRequest } = require('./lib/movies.js');
 const { handleWeatherRequest } = require('./lib/weather.js');
 
 
 const PORT = process.env.PORT;
 
-app.use(cors());
-app.use(express.json());
 
 // read the params, call the getWeather
 app.get('/weather/:lat_lon', handleWeatherRequest);
